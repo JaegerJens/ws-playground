b77450789c807a38ba33821e9d5af147527626ad
Jens Jaeger <jaegerj204@googlemail.com>
2017-08-06 11:21:41 +0200

    start ws and open ws connection

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require('http');
+const url = require('url');
 const WebSocket = require('ws');
 
 const port = 8080;
@@ -6,7 +8,17 @@ const app = express();
 
 app.use('/', express.static('static'));
 
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
 
+wss.on('connection', function connection(ws, req) {
+    const location = url.parse(req.url, true);
 
+    ws.on('message', function incoming(message) {
+        console.log(`received: ${message}`);
+    });
 
-app.listen(port, () => console.log(`Web server listening on http://localhost:${port}`));
+    ws.send('Hello world');
+});
+
+server.listen(port, () => console.log(`Web server listening on http://localhost:${port}`));
